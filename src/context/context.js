9a7bd7fc5b073014e28aabbb1b0adb864c68b0b1
yuyunfrancis <yuyun.francis@ictuniversity.org>
2022-02-16 13:25:27 +0100

    search functionality

--- src/context/context.js
+++ src/context/context.js
@@ -13,9 +13,76 @@ const GithubProvider = ({ children }) => {
   const [githubUser, setGithubUser] = useState(mockUser);
   const [repos, setRepos] = useState(mockRepos);
   const [followers, setFollowers] = useState(mockFollowers);
+  // request loadingGif
+  const [requests, setRequests] = useState(0);
+
+  const [isLoading, setIsLoading] = useState(false);
+  //error
+  const [error, setError] = useState({ show: false, msg: '' });
+
+  const searchGithubUser = async (user) => {
+    toggleError();
+    setIsLoading(true);
+    const response = await axios(`${rootUrl}/users/${user}`).catch((err) =>
+      console.log(err)
+    );
+
+    if (response) {
+      setGithubUser(response.data);
+      const { login, followers_url } = response.data;
+
+      //repos
+      axios(`${rootUrl}/users/${login}/repos?per_page=100`).then((response) =>
+        setRepos(response.data)
+      );
+
+      //followers
+      axios(`${followers_url}?per_page=100`).then((response) =>
+        setFollowers(response.data)
+      );
+    } else {
+      toggleError(true, 'user not found');
+    }
+    checkRequests();
+    setIsLoading(false);
+  };
+
+  //check rate_limit
+  const checkRequests = () => {
+    axios(`${rootUrl}/rate_limit`)
+      .then(({ data }) => {
+        let {
+          rate: { remaining },
+        } = data;
+
+        setRequests(remaining);
+
+        if (remaining === 0) {
+          toggleError(true, 'sorry you have exceeded your hourly rate limit!');
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
+  // error
+  function toggleError(show = false, msg = '') {
+    setError({ show, msg });
+  }
+
+  useEffect(checkRequests, []);
 
   return (
-    <GithubContext.Provider value={{ githubUser, repos, followers }}>
+    <GithubContext.Provider
+      value={{
+        githubUser,
+        repos,
+        followers,
+        requests,
+        error,
+        searchGithubUser,
+        isLoading,
+      }}
+    >
       {children}
     </GithubContext.Provider>
   );
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -3,10 +3,22 @@ import { Info, Repos, User, Search, Navbar } from '../components';
 import loadingImage from '../images/preloader.gif';
 import { GithubContext } from '../context/context';
 const Dashboard = () => {
+  const { isLoading } = React.useContext(GithubContext);
+
+  if (isLoading) {
+    return (
+      <main>
+        <Navbar />
+        <Search />
+        <img src={loadingImage} alt='loading' className='loading-img' />
+      </main>
+    );
+  }
+
   return (
     <main>
-      {/* <Navbar></Navbar>
-      <Search /> */}
+      <Navbar></Navbar>
+      <Search />
       <Info />
       <User />
       <Repos />
